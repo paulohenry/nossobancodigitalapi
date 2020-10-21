a9681547b0f05c087775cce2c1b6e072919bafd0
paulohenry <paulo.junior.engenheiro@gmail.com>
2020-10-20 23:08:37 -0300

    laste commit

--- src/main/java/com/paulohenry/zup/nbdigital/NbdigitalApplication.java
+++ src/main/java/com/paulohenry/zup/nbdigital/NbdigitalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NbdigitalApplication {
-
+ //initial
 	public static void main(String[] args) {
 		SpringApplication.run(NbdigitalApplication.class, args);
 	}
